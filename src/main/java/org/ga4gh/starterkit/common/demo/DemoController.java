c183b0998511db28219bcf7bc53da3691253faca
Jeremy Adams <jeremy.adams@ga4gh.org>
2021-06-07 13:10:47 -0400

    some unit testing of demo package

--- src/main/java/org/ga4gh/starterkit/common/demo/DemoController.java
+++ src/main/java/org/ga4gh/starterkit/common/demo/DemoController.java
@@ -1,6 +1,5 @@
 package org.ga4gh.starterkit.common.demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
---
+++ src/test/java/org/ga4gh/starterkit/common/demo/DemoControllerTest.java
@@ -0,0 +1,51 @@
+package org.ga4gh.starterkit.common.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class DemoControllerTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    private WebApplicationContext webAppContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeMethod
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+    }
+
+    @DataProvider(name = "cases")
+    public Object[][] getData() {
+        return new Object[][] {
+            {
+                "/hello-world",
+                "Hello, World! You have reached the PUBLIC 'hello-world' endpoint"
+            },
+            {
+                "/admin/hello-world",
+                "Hello, World! You have reached the ADMIN 'hello-world' endpoint"
+            }
+        };
+    }
+
+    @Test(dataProvider = "cases")
+    public void testDemoControllers(String requestPath, String expResponseBody) throws Exception {
+        MvcResult result = mockMvc.perform(get(requestPath)).andExpect(status().isOk()).andReturn();
+        String responseBody = result.getResponse().getContentAsString();
+        Assert.assertEquals(responseBody, expResponseBody);
+    }
+}
