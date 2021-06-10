8d0acf04186bb7e00935812a1e3e182df845140d
Jeremy Adams <jeremy.adams@ga4gh.org>
2021-06-10 08:49:19 -0400

    removed port from server props "hostname", URL port can be reconstructed from "publicApiPort" to avoid redundancy

--- src/main/java/org/ga4gh/starterkit/common/constant/ServerPropsDefaults.java
+++ src/main/java/org/ga4gh/starterkit/common/constant/ServerPropsDefaults.java
@@ -5,7 +5,7 @@ import org.ga4gh.starterkit.common.config.LogLevel;
 public class ServerPropsDefaults {
 
     public static final String SCHEME = "http";
-    public static final String HOSTNAME = "localhost:8080";
+    public static final String HOSTNAME = "localhost";
     public static final String PUBLIC_API_PORT = "4500";
     public static final String ADMIN_API_PORT = "4501";
     public static final LogLevel LOG_LEVEL = LogLevel.DEBUG;
